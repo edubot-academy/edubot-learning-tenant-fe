fae00c7a1e40ab3c609bba54bc1cf0e920a0987b
Bekten Orunbaev <beko_iuk@outlook.com>
2026-06-05 00:49:58 +0600

    Use single engagement theme stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,7 @@ import { LocaleProvider } from './i18n/LocaleProvider';
 import { App } from './app/App';
 import './i18n/config';
 import './styles/app.css';
-import './styles/engagement.css';
-import './styles/engagement-polish.css';
-import './styles/engagement-tone.css';
+import './styles/engagement-theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

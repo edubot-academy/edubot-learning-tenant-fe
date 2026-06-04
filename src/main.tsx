62e8fefa92709696330a8d37494fd6c8ef0f7878
Bekten Orunbaev <beko_iuk@outlook.com>
2026-06-04 23:46:59 +0600

    Load engagement theme styles

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import { LocaleProvider } from './i18n/LocaleProvider';
 import { App } from './app/App';
 import './i18n/config';
 import './styles/app.css';
+import './styles/engagement.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
